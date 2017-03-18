9c937c8bf76dc2289bb3f854c83d7cc4310b9476
Dishant Kapadiya <dishant@nyllabs.tech>
2017-03-18 16:50:53 -0400

    Created UI for the app

---
+++ SkyStatus/WeatherVC.swift
@@ -0,0 +1,40 @@
+//
+//  WeatherVC.swift
+//  SkyStatus
+//
+//  Created by Dishant Kapadiya on 3/18/17.
+//  Copyright © 2017 Dishant Kapadiya. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+
+}
+
+extension WeatherVC: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "cell")!
+    }
+}
