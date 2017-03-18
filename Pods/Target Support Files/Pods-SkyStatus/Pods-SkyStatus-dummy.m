7d8732c086d73c9d7c39f859314bfd708d04c857
Dishant Kapadiya <dishant@nyllabs.tech>
2017-03-18 16:50:06 -0400

    Basic Setup

---
+++ Pods/Target Support Files/Pods-SkyStatus/Pods-SkyStatus-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SkyStatus : NSObject
+@end
+@implementation PodsDummy_Pods_SkyStatus
+@end
---
+++ Pods/Target Support Files/Pods-SkyStatusUITests/Pods-SkyStatusUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SkyStatusUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SkyStatusUITests
+@end
